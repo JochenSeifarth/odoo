84409b00b15402ba97fa96599be42b21e98b5928
Jochen Seifarth <js@agentbob.info>
2026-03-15 17:21:43 +0100

    fix-up for local developments

--- copyRYA-ryadev.sh
+++ copyRYA-ryadev.sh
@@ -4,10 +4,8 @@ DUMPFILE=$(mktemp /tmp/rya.XXXXXX.zip)
 sudo chgrp odoo $DUMPFILE
 chmod 660 $DUMPFILE
 # odoo is particularly picky about position of options
-ssh rya "sudo -u odoo odoo db -c /etc/odoo/odoo.conf dump rya" 2> >(cat >&2) | pv -ptrab > $DUMPFILE
+ssh rya@rya "cd ~rya/git/odoo && ./venv_odoo/bin/python ./odoo-bin db -c ./rya-odoo-PROD.conf dump rya" 2> >(cat >&2) | pv -ptrab > $DUMPFILE
 # odoo is particularly picky about position of options
-###sudo -u odoo odoo db load --force --neutralize ryadev $DUMPFILE && rm -f $DUMPFILE
 cd /home/jochen/git/odoo
 . ./source venv_odoo/bin/activate 
-python3 odoo-bin.py db -c ./odoo.conf load --force --neutralize ryadev $DUMPFILE
-### (odoo_env) jochen@bruno:~/odoo$ ./odoo-bin db --db_host=127.0.0.1 --db_user odoo --db_password db_password load  --force --neutralize ryadev ryaPROD.zip
+python3 odoo-bin.py db -c ./rya-odoo-DEV.conf load --force --neutralize ryadev $DUMPFILE
