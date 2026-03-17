63a7e4a60610a16e67b3f19864fc96a34bac5f6d
Jochen Seifarth <js@agentbob.info>
2026-03-17 12:03:57 +0100

    deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-cd git/odoo
-git fetch; git reset --hard origin/19.0
+ssh rya@rya 
+cd git/odoo; git fetch; git reset --hard origin/19.0
 . venv_odoo/bin/activate
 sudo systemctl stop rya
-./odoo-bin -c /home/rya/git/odoo/rya-odoo-PROD.conf -u all --stop-after-init
+./odoo-bin -c /home/rya/git/odoo/rya-odoo-PROD.conf -u website_event_sale --stop-after-init
 sudo systemctl restart rya; tail -f /var/log/rya/rya-odoo-server.log
