da80de1f777daed767aa8ee8880da66aaab445d7
Jochen Seifarth <js@agentbob.info>
2026-03-25 16:29:36 +0100

    for title, use translated menu entry as website.page.name does not seem to be translated

--- addons/website/models/mixins.py
+++ addons/website/models/mixins.py
@@ -45,7 +45,10 @@ class WebsiteSeoMetadata(models.AbstractModel):
         self.ensure_one()
         company = request.website.company_id.sudo()
         title = request.website.name
-        if 'name' in self:
+        page = self.page_ids[:1] if 'page_ids' in self._fields else None
+        if page and page.menu_ids:
+            title = '%s | %s' % (page.menu_ids[0].name, title)
+        elif 'name' in self:
             title = '%s | %s' % (self.name, title)
 
         img_field = 'social_default_image' if request.website.has_social_default_image else 'logo'
--- copyRYA-ryadev.sh
+++ copyRYA-ryadev.sh
@@ -7,5 +7,5 @@ chmod 660 $DUMPFILE
 ssh rya@rya "cd ~rya/git/odoo && ./venv_odoo/bin/python ./odoo-bin db -c ./rya-odoo-PROD.conf dump rya" 2> >(cat >&2) | pv -ptrab > $DUMPFILE
 # odoo is particularly picky about position of options
 cd /home/jochen/git/odoo
-. ./source venv_odoo/bin/activate 
+. ./venv_odoo/bin/activate 
 python3 odoo-bin.py db -c ./rya-odoo-DEV.conf load --force --neutralize ryadev $DUMPFILE
