169cc62a1e420585283bbb0f0172e172ab6e7554
Jochen Seifarth <js@agentbob.info>
2026-06-18 18:07:20 +0200

    HOTFIX: Don't require NIF (VAT) for ES addresses

--- addons/l10n_es/controllers/portal.py
+++ addons/l10n_es/controllers/portal.py
@@ -18,8 +18,9 @@ class L10nESPortalAccount(PortalAccount):
         """Require VAT/NIF for Spanish customers in billing addresses on Spanish e-commerce."""
         field_names = super()._get_mandatory_billing_address_fields(country_sudo)
 
-        if request.env.company.country_code == country_sudo.code == 'ES':
-            field_names.add('vat')
+        ### Don't required VAT/NIF for B2C invoices, as it is not mandatory for them.
+        ### if request.env.company.country_code == country_sudo.code == 'ES':
+        ###    field_names.add('vat')
 
         return field_names
 
